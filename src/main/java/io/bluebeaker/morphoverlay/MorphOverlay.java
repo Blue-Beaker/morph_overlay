4564a25db9422f7649bdeafb1b20a158dfa1817f
blue-beaker <yyy5566778899@126.com>
2025-08-17 16:40:49 +0800

    init

---
+++ src/main/java/io/bluebeaker/morphoverlay/MorphOverlay.java
@@ -0,0 +1,51 @@
+package io.bluebeaker.morphoverlay;
+
+import org.apache.logging.log4j.Logger;
+
+import io.bluebeaker.morphoverlay.Tags;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.common.config.Config.Type;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class MorphOverlay
+{
+    public static final String MODID = Tags.MOD_ID;
+    public static final String NAME = Tags.MOD_NAME;
+    public static final String VERSION = Tags.VERSION;
+    
+    public MinecraftServer server;
+
+    private static Logger logger;
+    
+    public MorphOverlay() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+    @EventHandler
+    public void onServerStart(FMLServerStartingEvent event){
+        this.server=event.getServer();
+    }
+
+    @SubscribeEvent
+    public void onConfigChangedEvent(OnConfigChangedEvent event) {
+        if (event.getModID().equals(MODID)) {
+            ConfigManager.sync(MODID, Type.INSTANCE);
+        }
+    }
+
+    public static Logger getLogger(){
+        return logger;
+    }
+}
---
+++ src/main/java/io/bluebeaker/morphoverlay/MorphOverlayConfig.java
@@ -0,0 +1,13 @@
+package io.bluebeaker.morphoverlay;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.LangKey;
+import net.minecraftforge.common.config.Config.Type;
+
+@Config(modid = MorphOverlay.MODID,type = Type.INSTANCE,category = "general")
+public class MorphOverlayConfig {
+    @Comment("Example")
+    @LangKey("config.morphoverlay.example.name")
+    public static boolean example = true;
+}
